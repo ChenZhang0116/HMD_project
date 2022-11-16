7c514e9626673c02b5078c13b3b21a07b38e344d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-16 05:47:04 +0000

    Adding tarfile member sanitization to extractall()

--- testrun.py
+++ testrun.py
@@ -27,7 +27,26 @@ test_path = data_dir / "test_unseen.jsonl"
 # First let's extract the images if we haven't already:
 if not (data_dir / "img").exists():
     with tarfile.open(img_tar_path) as tf:
-        tf.extractall(data_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tf, data_dir)
 
 train_samples_frame = pd.read_json(train_path, lines=True)
 print(train_samples_frame.head())
